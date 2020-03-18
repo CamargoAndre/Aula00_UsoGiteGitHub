edfc74f5a449bbde8bfe056a6f944f7359360f1a
Yuri <yuri_pinheiro1402@hotmail.com>
2020-03-18 19:26:40 -0300

    Setters

--- Aluno.java
+++ Aluno.java
@@ -1,16 +1,23 @@
-public class Aluno{
-   private String nome;
-   private int id;
+public class Aluno {
+	private String nome;
+	private int id;
 
-   
-   // Getters
-   public String getNome(){
-	return nome;
-   }
-  
-   public int getId(){
-	return id;
-   }
-   
+	// Getters
+	public String getNome() {
+		return nome;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	// Setters
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 }
-   
