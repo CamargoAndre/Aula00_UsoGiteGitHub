f045ab8398a1f199b7d64dd32fcca1de3acf081a
Yuri <yuri_pinheiro1402@hotmail.com>
2020-03-18 20:33:08 -0300

    Setters branch_yuri

--- Aluno.java
+++ Aluno.java
@@ -2,26 +2,6 @@ public class Aluno {
 	private String nome;
 	private int id;
 	
-	public Aluno(String nome, int id) {
-		this.nome = nome;
-		this.id = id;
-	}
-
-	public Aluno(String nome, int id){
-		nome = nome;
-		id = id;
-
-	}
-
-	// Getters
-	public String getNome() {
-		return nome;
-	}
-
-	public int getId() {
-		return id;
-	}
-	
 	// Setters
 	public void setNome(String nome) {
 		this.nome = nome;
