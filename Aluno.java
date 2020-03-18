4aa7aa5172f2d7e33cb2fe4af03315b4c6bdbefc
Andre de Camargo <camargo_and@hotmail.com>
2020-03-18 18:25:16 -0300

    Insercao Get

--- Aluno.java
+++ Aluno.java
@@ -1,6 +1,16 @@
 public class Aluno{
    private String nome;
    private int id;
+
+   
+   // Getters
+   public String getNome(){
+	return nome;
+   }
+  
+   public int getId(){
+	return id;
+   }
    
 }
    
