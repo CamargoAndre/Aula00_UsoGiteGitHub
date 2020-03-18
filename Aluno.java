e0166a6fa1a69b56b3af8ce0a94614c1a5884223
Andre de Camargo <camargo_and@hotmail.com>
2020-03-18 19:39:10 -0300

    Construtor sem this

--- Aluno.java
+++ Aluno.java
@@ -2,6 +2,12 @@ public class Aluno {
 	private String nome;
 	private int id;
 
+	public Aluno(String nome, int id){
+		nome = nome;
+		id = id;
+
+	}
+
 	// Getters
 	public String getNome() {
 		return nome;
