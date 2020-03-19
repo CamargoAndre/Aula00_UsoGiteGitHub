f128c316afa5384eaf7553abc1571e5a0eb19eee
Andre de Camargo <camargo_and@hotmail.com>
2020-03-18 21:07:57 -0300

    Versao com o Getter/Setters

--- Aula00_Eclipse/src/Aluno.java
+++ Aula00_Eclipse/src/Aluno.java
@@ -3,13 +3,24 @@ public class Aluno {
 	private String nome;
 	private int id;
 
-	// Get
+	// Getters
 	public String getNome() {
 		return nome;
 	}
 
 	public int getId() {
 		return id;
+	}
+	
+	//Setters
+	public void setNome(String nome) {
+		
+		this.nome = nome;
+	}
+	
+	public void setId(int id) {
+		
+		this.id = id;
 	}
 	
 }
